846ffddae04ea37365537dcaa02b63b28883ad41
dhiyanesh <dhiyaneshsasi03@gmail.com>
2024-07-22 10:33:02 +0530

    Sign Up page update

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+    return (
+        <div className="flex items-center justify-center flex-col gap-10">
+            <h1 className="text-2xl font-semibold mt-10">Sign-in Page</h1>
+            <SignUp />
+        </div>
+    )
 }
